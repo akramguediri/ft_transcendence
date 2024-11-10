8c6460a66820ed0119ec6df8c164db7c379b56ef
Milad Ahmadi Solem <miahmadi@student.42heilbronn.de>
2024-11-10 23:51:20 +0100

    Added csrf cookie decorator for index function;

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,7 @@
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_protect, ensure_csrf_cookie
 
 
+@ensure_csrf_cookie
 def index(request):
     return JsonResponse({'message': 'test'})
