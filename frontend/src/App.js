bdd579edb1551c74c2aab7d6247bba9f685821eb
Milad Ahmadi Solem <miahmadi@student.42heilbronn.de>
2024-11-10 23:52:40 +0100

    Set up a sandbox default page to test GET and POST requests

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+async function getData() {
+
+	const response = await fetch('http://127.0.0.1:8000/api/', {
+		mode:  'cors',
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+			},
+	})
+	console.log(await response.json());
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [ POSTElement, setPOSTElement ] = useState('');
+	const [ GETElement, setGETElement ] = useState('GET-Result: ');
+
+	return (
+		<div className="App">
+			<header className="App-header">
+
+				<div>
+
+					<p> Fetch GET - tester </p>
+					<p> <button onClick = { (e) => getData(e) } > GET test</button> </p>
+					<p> <button onClick = { (e) => setGETElement('GET-Result: Success!') } > GET </button> </p>
+					<p className="fetchResult" > { GETElement } </p>
+
+					<p> Fetch POST - tester </p>
+					<input type="text" value= { POSTElement } />
+					<input type="button" value="POST" onClick = { (e) => setPOSTElement('POST-Result: Success!') } />
+
+				</div>
+
+			</header>
+		</div>
+	);
 }
 
 export default App;
