dd6d7ab11e45091460092c2158b758d16524ff20
Milad Ahmadi Solem <miahmadi@student.42heilbronn.de>
2024-11-11 02:12:37 +0100

    Adjusted settings for django-cors-headers

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -129,9 +129,11 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-        'http://127.0.0.1:3000',
+    'http://127.0.0.1:3000',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-        'http://127.0.0.1:3000',
+    'http://127.0.0.1:3000',
 ]
+
+CORS_ALLOW_CREDENTIALS = True
