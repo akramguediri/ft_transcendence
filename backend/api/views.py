bd1ee2c2ccc5b76aecdd4bd24760bc749e5c5d28
Milad Ahmadi Solem. <miahmadi@student.42heilbronn.de>
2024-11-11 02:12:11 +0100

    Implemented getCSRFToken endpoint for cors policy issues;

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,11 @@
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_protect, ensure_csrf_cookie
+from django.views.decorators.csrf import ensure_csrf_cookie
+from django.middleware.csrf import get_token
 
+def csrf(request):
+    return JsonResponse({'csrfToken': get_token(request)})
 
-@ensure_csrf_cookie
 def index(request):
-    return JsonResponse({'message': 'test'})
+    if request.method == "GET":
+        return JsonResponse({'message': 'GETResponse'})
+    return JsonResponse({'message': 'POSTResponse'})
