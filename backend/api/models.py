e17bca250dc24d1cb581b0b8779432bc832bc7f6
madou9 <issoufou.mamadou@gmail.com>
2024-11-22 16:23:00 +0100

    add models for user register

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
-# Create your models here.
 class Student(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(blank=True, max_length=30)
+    first_name = models.CharField(blank=True, max_length=30)
+    last_name = models.CharField(blank=True, max_length=30)
+    email = models.CharField(blank=True, max_length=64)
+    password = models.CharField(blank=True, max_length=30)
+
