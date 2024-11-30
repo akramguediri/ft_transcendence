710f358e19e685b61fae64cc9757be0249420f84
Mounir Tayebi <mounirtayebi1@gmail.com>
2024-11-30 19:23:34 +0100

    added updateName view url

--- backend/usermanagement/urls.py
+++ backend/usermanagement/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("csrf", views.csrf, name="csrf"),
     path("register", views.registerUser, name="registerUser"),
     path("login", views.loginUser, name="loginUser"),
+    path("updateName", views.updateName, name="updateName"),
 ]
