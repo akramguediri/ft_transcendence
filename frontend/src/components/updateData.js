17a9c9645a3422cba1f85699312fba0d1e5cee26
adinari42 <anasdinari@gmail.com>
2024-12-13 22:09:36 +0100

    removed extra unused parameter in fetch of updateName endpoint

--- frontend/src/components/updateData.js
+++ frontend/src/components/updateData.js
@@ -1,13 +1,12 @@
 import GetCSRFToken from './getCSRFToken';
 
-async function UpdateName({name, new_name}) {
+async function UpdateName({ new_name }) {
 	const response = await fetch('http://127.0.0.1:8000/usermanagement/updateName', {
 		method: 'POST',
 		credentials: 'include',
 		body: JSON.stringify({
-				name : name,
-				new_name : new_name,
-			}),
+			new_name: new_name,
+		}),
 		headers: {
 			'X-CSRFToken': await GetCSRFToken(),
 			'Content-Type': 'application/json'
@@ -16,13 +15,13 @@ async function UpdateName({name, new_name}) {
 	const data = await response.json()
 
 
-    if (data.status === 'success') {
-        const user = JSON.parse(localStorage.getItem('user'));
-        if (user) {
-            user.name = new_name;
-            localStorage.setItem('user', JSON.stringify(user));
-        }
-    }
+	if (data.status === 'success') {
+		const user = JSON.parse(localStorage.getItem('user'));
+		if (user) {
+			user.name = new_name;
+			localStorage.setItem('user', JSON.stringify(user));
+		}
+	}
 
 	return data;
 }
