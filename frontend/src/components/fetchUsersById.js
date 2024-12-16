685791d35c00d8b0c1e1e149f3e72fa41109fa3d
adinari42 <anasdinari@gmail.com>
2024-12-16 04:09:10 +0100

    code refactor and fixes

---
+++ frontend/src/components/fetchUsersById.js
@@ -0,0 +1,23 @@
+import GetCSRFToken from './getCSRFToken';
+
+async function fetchUsersById(userId) {
+	const response = await fetch('http://127.0.0.1:8000/usermanagement/fetchUserById', {
+		method: 'POST',
+		credentials: 'include',
+		body: JSON.stringify({
+			user_id: userId,
+		}),
+		headers: {
+			'X-CSRFToken': await GetCSRFToken(),
+			'Content-Type': 'application/json',
+		},
+	});
+	const data = await response.json();
+	if (data.status !== 'success') {
+		return { status: 'error', msg: data.msg || 'Failed to fetch user.' };
+	}
+	return { status: 'success', data };
+}
+
+export default fetchUsersById;
+
