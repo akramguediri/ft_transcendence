41acba49e5a394ff03d5d8c0dbfc84e17d6fed5d
madou9 <issoufou.mamadou@gmail.com>
2024-12-20 23:00:10 +0100

    added picture for Team developer

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -1,5 +1,8 @@
 import hero_42 from './hero_42.jpg';
-
+import hama_picture from './Hama.PNG';
+import anass_picture from './Anass_picture.jpg';
 export {
     hero_42,
+    hama_picture,
+    anass_picture
 }
