9f4136200660e5679fb8fe94f1ae36f92cc69fe1
madou9 <issoufou.mamadou@gmail.com>
2024-12-22 20:18:16 +0100

    added pictures

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -1,8 +1,14 @@
 import hero_42 from './hero_42.jpg';
 import hama_picture from './Hama.PNG';
 import anass_picture from './Anass_picture.jpg';
+import akram_picture from './Akram_picture.jpg';
+import milad_picture from './Milad_picture.jpeg';
+
+
 export {
     hero_42,
     hama_picture,
-    anass_picture
+    anass_picture,
+    akram_picture,
+    milad_picture
 }
