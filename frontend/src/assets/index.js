440668a227007188fd68ad8fdcd30b2b504707d8
madou9 <issoufou.mamadou@gmail.com>
2025-01-14 13:56:41 +0100

    remove picture

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -1,6 +1,6 @@
 import hero_42 from './hero_42.jpg';
 import hama_picture from './Hama.PNG';
-import anass_picture from './Anass_picture.jpg';
+import pong from './pong.png';
 import akram_picture from './Akram_picture.jpg';
 import milad_picture from './Milad_picture.jpeg';
 
@@ -8,7 +8,7 @@ import milad_picture from './Milad_picture.jpeg';
 export {
     hero_42,
     hama_picture,
-    anass_picture,
+    pong,
     akram_picture,
     milad_picture
 }
