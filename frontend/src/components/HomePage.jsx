2a898b7b7314cce0c9082ffa8a12067a43a5b554
madou9 <issoufou.mamadou@yahoo.com>
2025-01-17 20:20:57 +0100

    added useEffect to be able to send the code generated from the 42 api to send it to the backend

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { akram_picture, anass_picture, hama_picture, hero_42, milad_picture } from '../assets';
 import Navbar from './Navbar';
 import styles from '../styles.css';
+import Get42Token from './user_management/Get42Token';
+import { useNavigate } from 'react-router-dom';
 
 const socialLinks = [
   { platform: 'linkedin', icon: 'bi bi-linkedin' },
@@ -31,62 +33,73 @@ const TeamCard = ({ image, name, description }) => (
   </div>
 );
 
-const HomePage = () => (
-  <>
-    {/* Navbar */}
-    <header className="py-4">
-      <Navbar />
-    </header>
+const HomePage = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const code = urlParams.get('code');
 
-    {/* Hero Section */}
-    <section id="toff">
-      <div className="background-overlay">
-        <div className="text-center">
-          <h1>Welcome to our project ft_transcendence</h1>
-        </div>
-      </div>
-    </section>
+    if (code) {
+        Get42Token(code)
+    }
+}, [navigate]);
+  return (
+    <>
+      {/* Navbar */}
+      <header className="py-4">
+        <Navbar />
+      </header>
 
-    {/* Team Section */}
-    <section id="auteur" className="py-5 bg-light">
-      <div className="container">
-        {/* Team Intro */}
-        <div className="row text-center mb-4">
-          <div className="col">
-            <h1 className="text-primary">The Team</h1>
-            <p>
-              Meet our amazing team dedicated to building ft_transcendence. Lorem ipsum dolor sit amet, consectetur
-              adipisicing elit. Dignissimos minima fuga quaerat provident itaque quas eligendi praesentium.
-            </p>
+      {/* Hero Section */}
+      <section id="toff">
+        <div className="background-overlay">
+          <div className="text-center">
+            <h1>Welcome to our project ft_transcendence</h1>
           </div>
         </div>
+      </section>
 
-        {/* Team Members */}
-        <div className="row">
-          <TeamCard
-            image={milad_picture}
-            name="Milad"
-            description="Milad is a passionate developer with expertise in fullstack development."
-          />
-          <TeamCard
-            image={hama_picture}
-            name="Ihama"
-            description="Ihama specializes in Frontend development, ensuring a seamless user experience for all users."
-          />
-          <TeamCard
-            image={akram_picture}
-            name="Akram"
-            description="Akram is our backend wizard."
-          />
-          <TeamCard
-            image={anass_picture}
-            name="Anass"
-            description="Anass is the go-to person for DevOps and ensuring smooth deployments."
-          />
+      {/* Team Section */}
+      <section id="auteur" className="py-5 bg-light">
+        <div className="container">
+          {/* Team Intro */}
+          <div className="row text-center mb-4">
+            <div className="col">
+              <h1 className="text-primary">The Team</h1>
+              <p>
+                Meet our amazing team dedicated to building ft_transcendence. Lorem ipsum dolor sit amet, consectetur
+                adipisicing elit. Dignissimos minima fuga quaerat provident itaque quas eligendi praesentium.
+              </p>
+            </div>
+          </div>
+
+          {/* Team Members */}
+          <div className="row">
+            <TeamCard
+              image={milad_picture}
+              name="Milad"
+              description="Milad is a passionate developer with expertise in fullstack development."
+            />
+            <TeamCard
+              image={hama_picture}
+              name="Ihama"
+              description="Ihama specializes in Frontend development, ensuring a seamless user experience for all users."
+            />
+            <TeamCard
+              image={akram_picture}
+              name="Akram"
+              description="Akram is our backend wizard."
+            />
+            <TeamCard
+              image={anass_picture}
+              name="Anass"
+              description="Anass is the go-to person for DevOps and ensuring smooth deployments."
+            />
+          </div>
         </div>
-      </div>
-    </section>
-  </>
-);
+      </section>
+    </>
+  )
+};
 
 export default HomePage;
