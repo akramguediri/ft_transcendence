cfd390737b6f8c5b9a98280b411be69fe7d5a929
madou9 <issoufou.mamadou@gmail.com>
2025-03-09 19:06:24 +0100

    export game picture

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -1,6 +1,6 @@
 import hero_42 from './hero_42.jpg';
 import hama_picture from './Hama.PNG';
-import game from './game.png';
+import game from './game.webp';
 import akram_picture from './Akram_picture.jpg';
 import milad_picture from './Milad_picture.jpeg';
 
