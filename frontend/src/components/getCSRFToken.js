36839943ead365a4f8ea1874fa5aca5113afe76c
madou9 <issoufou.mamadou@gmail.com>
2025-03-11 03:56:57 +0100

    implemented the way that after get the token to store it in the cookies to avoid called each time token

--- frontend/src/components/getCSRFToken.js
+++ frontend/src/components/getCSRFToken.js
@@ -1,13 +1,11 @@
-
 async function GetCSRFToken() {
-
-		const response = await fetch(`http://127.0.0.1:8000/usermanagement/csrf`, {
-			method: 'GET',
-			credentials: 'include', // Important for sending/receiving cookies
-		});
-		const data = await response.json();
-		document.cookie = "csrftoken=" + data.csrfToken;
-	return data.csrfToken;
+    const response = await fetch(`http://127.0.0.1:8000/csrf`, {
+        method: 'GET',
+        credentials: 'include', // Important for sending/receiving cookies
+    });
+    const data = await response.json();
+    document.cookie = `csrftoken=${data.csrfToken}; path=/;`;
+    return data.csrfToken;
 }
 
 export default GetCSRFToken;
