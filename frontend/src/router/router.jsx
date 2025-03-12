3f2c5aa1a720ab7b1bface550fa7cfb8c0f4bdad
madou9 <issoufou.mamadou@yahoo.com>
2025-03-13 00:10:09 +0100

    added route for Game page

--- frontend/src/router/router.jsx
+++ frontend/src/router/router.jsx
@@ -5,6 +5,7 @@ import LoginUser from "../components/user_management/LoginUser";
 import Profile from "../components/user_management/Profile";
 import InvitationFriends from "../components/user_management/invitations/InvitationFriends";
 import FetchUserById from "../components/user_management/FetchUserById";
+import GamePage from "../components/gameUI/GamePage";
 
 
 export const router = createBrowserRouter([
@@ -31,5 +32,9 @@ export const router = createBrowserRouter([
     {
         path: 'fetch-user',
         element: <FetchUserById />,
+    },
+    {
+        path: 'game',
+        element: <GamePage />,
     }
 ]);
