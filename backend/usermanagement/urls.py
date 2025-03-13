5a83e74db24ef6d68dcc829f335a81e13cd7611b
madou9 <issoufou.mamadou@yahoo.com>
2025-03-13 02:55:04 +0100

    add path for getToken

--- backend/usermanagement/urls.py
+++ backend/usermanagement/urls.py
@@ -19,5 +19,6 @@ urlpatterns = [
     path('blockuser', views.block_user, name='block_user'),
     path('unblockuser', views.unblock_user, name='unblock_user'),
     path('fetchblockedusers', views.fetch_blocked_users, name='fetch_blocked_users'),
+    path('get42token', views.getToken, name='get_42_token'),
 ]
 
