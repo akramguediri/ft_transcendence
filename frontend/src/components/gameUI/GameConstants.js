c33df14fbdc3f2d57ba2e046beac5661734b4e1c
akramguediri <akramguediri2@gmail.com>
2025-03-14 04:51:07 +0100

    constants

---
+++ frontend/src/components/gameUI/GameConstants.js
@@ -0,0 +1,8 @@
+// src/components/GamePage/GameConstants.js
+export const GAME_WIDTH = 800;
+export const GAME_HEIGHT = 500;
+export const PADDLE_WIDTH = 15;
+export const PADDLE_HEIGHT = 100;
+export const BALL_SIZE = 15;
+export const PADDLE_SPEED = 8;
+export const AI_DIFFICULTY = 0.7; // Adjust between 0 and 1 for AI difficulty
