67844826e69dd1bb4248f44fe2e2a9d53f3af73c
madou9 <issoufou.mamadou@yahoo.com>
2025-03-16 03:44:45 +0100

    Retrieve the authenticated user's data from localStorage

--- frontend/src/components/gameUI/useGameRecords.js
+++ frontend/src/components/gameUI/useGameRecords.js
@@ -2,6 +2,15 @@ import API_URL from '../config.js';
 
 export const useGameRecords = () => {
     const saveGameRecord = async (gameState, constants, gameMode, gameId, player1, player2) => {
+        // Retrieve the authenticated user's data from localStorage
+        const userData = JSON.parse(localStorage.getItem('user'));
+        if (!userData || !userData.user_name) {
+            console.error('Authenticated user data not found in localStorage');
+            return;
+        }
+        // Set player1 to the authenticated user's username
+        player1 = userData.user_name;
+
         // Determine the result, winner, and loser
         const result = `${gameState.score1}-${gameState.score2}`;
         const winner = gameState.score1 > gameState.score2 ? player1 : player2;
@@ -24,7 +33,6 @@ export const useGameRecords = () => {
             },
             timestamp: new Date().toISOString(),
         };
-
         try {
             // Retrieve the CSRF token from cookies
             const csrfToken = getCookie("csrftoken");
